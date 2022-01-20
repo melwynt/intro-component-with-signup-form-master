71f41f308b1ca9047443a6298a0112b1f259cd46
Melwyn Turbant <melwynt@gmail.com>
2022-01-20 18:23:11 +0100

    :sparkles: Refactor with React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.js';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+if (module.hot) {
+  module.hot.accept();
+}
